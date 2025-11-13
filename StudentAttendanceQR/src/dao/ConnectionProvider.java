b67f0035b9ee55d842e4038488f7875c8fc76cf4
Manisha Patil <161874312+Manisha-9921@users.noreply.github.com>
2025-11-13 16:03:48 +0530

    Change database password in ConnectionProvider
    Updated the database password in ConnectionProvider.java.

--- StudentAttendanceQR/src/dao/ConnectionProvider.java
+++ StudentAttendanceQR/src/dao/ConnectionProvider.java
@@ -16,7 +16,7 @@ public class ConnectionProvider {
     private static final String DB_NAME = "AttendanceSystem";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "Patil@123";
+    private static final String DB_PASSWORD = "yourpassword";
 
     public static Connection getCon(){
         try{
@@ -44,3 +44,4 @@ public class ConnectionProvider {
         System.out.println("Database '" + dbName + "' created successfully.");
     }
 }
+
